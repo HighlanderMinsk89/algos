790539e423a9a1cf41019584fea5502ba8990526
Slava Adnavorau <slava.adnavorau@gmail.com>
2020-03-03 13:27:17 -0500

    BS

--- BinarySearch.js
+++ BinarySearch.js
@@ -1,16 +1,14 @@
 function binarySearch(array, target) {
   let left = 0,
-    right = array.length - 1;
-  let middle = Math.floor(array.length / 2);
-  while (left !== right) {
+    right = array.length - 1,
+    middle;
+  while (left <= right) {
+    middle = Math.floor((left + right) / 2);
     if (array[middle] === target) return middle;
     else if (array[middle] > target) {
       right = middle - 1;
-      middle = Math.floor(array.slice(0, middle).length / 2);
     } else {
       left = middle + 1;
-      middle = Math.floor(array.slice(left).length / 2);
-      if (array[left] === target) return left;
     }
   }
   return -1;
